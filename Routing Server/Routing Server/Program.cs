ecd10d284da2fc6612825a753cbccf552723939d
LoicLeBris <loic.le-bris@etu.univ-cotedazur.fr>
2022-11-21 19:28:06 +0100

    code non fonctionnel

--- Routing Server/Routing Server/Program.cs
+++ Routing Server/Routing Server/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Device.Location;
 using System.Collections;
 using System.Web;
+using System.Security.Policy;
 
 namespace Routing_Server
 {
@@ -19,9 +20,9 @@ namespace Routing_Server
         static void Main(string[] args)
         {
 
-            string url = "https://api.jcdecaux.com/vls/v3/";
+            string bikeUrl = "https://api.jcdecaux.com/vls/v3/";
             string query = "contracts?apiKey=a20510ebde21e2f45630b65733ea766ea9a88778";
-            string response = callApi(url, query).Result;
+            string response = callApi(bikeUrl, query).Result;
 
             List<Contracts> contracts = JsonSerializer.Deserialize<List<Contracts>>(response);
 
@@ -34,41 +35,43 @@ namespace Routing_Server
             string chosenContract = Console.ReadLine();
 
             string queryStation = "stations?contract=" + chosenContract + "&apiKey=a20510ebde21e2f45630b65733ea766ea9a88778";
-            string responseStations = callApi(url, queryStation).Result;
-
-            string adress = "186 chemin des génissieux Saint-Ismier";
-
-            string encodedAdress = HttpUtility.UrlEncode(adress, );
-
+            string responseStations = callApi(bikeUrl, queryStation).Result;
 
             List<Station> stations = JsonSerializer.Deserialize<List<Station>>(responseStations);
 
-            double myLatitude = 43.619214;
-            double myLongitude = 7.0760172;
-            GeoCoordinate myPos = new GeoCoordinate(myLatitude, myLongitude);
+            Console.WriteLine("Please enter an adress of origin :");
+            string originAdress = Console.ReadLine();
+
+            string encodedAdress = HttpUtility.UrlEncode(originAdress);
+
+            string positionAdress = callApi("https://nominatim.openstreetmap.org/", "search?q=" + encodedAdress + "&format=json&polygon=1&addressdetails=1").Result;
+
+            Console.WriteLine(positionAdress);
+
+            //GeoCoordinate myPos = new GeoCoordinate(myLatitude, myLongitude);
 
 
-            List<double> distances = new List<double>();
+            /*List<double> distances = new List<double>();
             foreach (Station station in stations)
             {
                 GeoCoordinate stationPos = new GeoCoordinate(station.position.latitude, station.position.longitude);
-                double distance = stationPos.GetDistanceTo(myPos);
-                distances.Add(distance);
-            }
+                //double distance = stationPos.GetDistanceTo(myPos);
+                //distances.Add(distance);
+            }*/
 
-            double shortest_distance = distances.Min();
-            Console.WriteLine("Shortest distance " + shortest_distance);
+            /*double shortest_distance = distances.Min();
+            Console.WriteLine("Shortest distance " + shortest_distance);*/
 
-            int i;
+            /*int i;
             for (i = 0; i < distances.Count; i++)
             {
                 if (distances[i] == shortest_distance)
                 {
                     break;
                 }
-            }
+            }*/
 
-            Console.WriteLine("La station la plus proche est :" + stations[i].name);
+            //Console.WriteLine("La station la plus proche est :" + stations[i].name);
 
             Console.ReadLine();
 
