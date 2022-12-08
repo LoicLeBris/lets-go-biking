f8021d69b944ed6e9e104f21778a1cde4912e704
LoicLeBris <loic.le-bris@etu.univ-cotedazur.fr>
2022-12-08 19:10:45 +0100

    derniers changements pour de vrai

--- HeavyClient/src/main/java/Main.java
+++ HeavyClient/src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.client.ServiceLetsGoBiking;
 
 import java.util.Scanner;
 
+import com.sun.xml.ws.fault.ServerSOAPFaultException;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
@@ -22,22 +23,32 @@ public class Main implements MessageListener {
     static Session session;
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Adresse de départ :");
-        String origin = scanner.nextLine();
-        System.out.println("Adresse d'arrivée :");
-        String destination = scanner.nextLine();
-
-        new Main().launchActiveMq();
-
-        client.getItinerary(origin, destination);
-        
         try {
-            session.close();
-            connection.close();
-        } catch (Exception ex) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Adresse de départ :");
+            String origin = scanner.nextLine();
+            System.out.println("Adresse d'arrivée :");
+            String destination = scanner.nextLine();
+
+            new Main().launchActiveMq();
+
+            client.getItinerary(origin, destination);
+
+            scanner.nextLine();
+
+            System.out.println("Do you want to enter another itinerary ? (y/n)");
+
+            if(scanner.nextLine().equals("y")){
+                main(null);
+            }
+            else{
+                session.close();
+                connection.close();
+            }
+
+        } catch (ServerSOAPFaultException | JMSException e) {
             System.out.println("Exception Occured");
+            main(null);
         }
     }
 
